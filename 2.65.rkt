8a4975ceb453c1bb2cb38d47a9012992b7b7faa7
Johan Swanljung <jswanljung@users.noreply.github.com>
2023-02-03 22:27:57 +0100

    Done with 2.73, some reformatting in other files

--- 2.65.rkt
+++ 2.65.rkt
@@ -29,14 +29,14 @@
                       remaining-elts))))))))
 
 #;(define (element-of-set? x set)
-  (cond ((null? set) false)
-        ((= x (car set)) true)
-        ((< x (car set)) false)
-        (else (element-of-set? x (cdr set)))))
+    (cond ((null? set) false)
+          ((= x (car set)) true)
+          ((< x (car set)) false)
+          (else (element-of-set? x (cdr set)))))
 
 (define (intersection-set set1 set2)
   (if (or (null? set1) (null? set2))
-      '()    
+      '()
       (let ((x1 (car set1)) (x2 (car set2)))
         (cond ((= x1 x2)
                (cons x1
@@ -48,12 +48,12 @@
                (intersection-set set1 (cdr set2)))))))
 
 #;(define (adjoin-set x set)
-  (if (null? set) (list x)
-      (let ((y (car set)))
-        (cond ((= x y) set)
-              ((> x y)
-               (cons y (adjoin-set x (cdr set))))
-              (else (cons x set))))))
+    (if (null? set) (list x)
+        (let ((y (car set)))
+          (cond ((= x y) set)
+                ((> x y)
+                 (cons y (adjoin-set x (cdr set))))
+                (else (cons x set))))))
 
 (define (union-set set1 set2)
   (cond ((and (null? set1) (null? set2)) '())
